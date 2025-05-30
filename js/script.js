13f6e9046aba4000704e262109f2ba51231114ec
Zeldachild <139978532+Zeldachild@users.noreply.github.com>
2025-05-30 19:32:58 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Initialize AOS animations
+AOS.init({
+  once: true,         // Animate only once when scrolling down
+  duration: 800,      // Animation duration in milliseconds
+  offset: 120         // Offset from the original trigger point
+});
+
+
+// Initialize AOS animations
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    once: true,
+    duration: 800,
+    easing: 'ease-in-out',
+  });
+});
+
